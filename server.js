bb4b8a1c399c3e6690ea907bd66cc9ff62e851dd
k.dubov <email@example.email>
2018-10-09 17:45:13 +0300

    Настройка Express сервера

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var tasks = require('./routes/tasks');
+// var index = require('./routes/index');
+// var tasks = require('./routes/tasks');
+var clietnPath = path.join(__dirname);
 
 var port = 3000;
 
 var app = express();
+app.use(express.static(clietnPath))
+
+app.listen(port, ()=> {
+    console.log(`port ${port}`);
+    
+})
